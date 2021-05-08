b56e2350638993c9cdaaaf2e8ecb565697fe0167
EC2 Default User <ec2-user@ip-172-31-2-95.us-east-2.compute.internal>
2021-05-08 05:50:32 +0000

    new

---
+++ main.js
@@ -0,0 +1,23 @@
+
+function update ( _v )
+{
+    document.querySelector( 'input' ).value = _v
+}
+
+function append( _v )
+{
+    document.querySelector( 'input' ).value += _v
+}
+
+
+function calc() 
+    {
+        const v = document.querySelector( 'input' ).value
+        try {
+            const f = new Function( 'return ' + v )
+            update( f().toString() )
+        } catch( _error ) {
+            update( _error )
+        }
+    }
+
